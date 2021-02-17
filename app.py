d555b25c762061454565f50e2a028f612fea9e68
NFuller721 <nfuller7218@gmail.com>
2021-02-16 22:36:19 -0500

    Deleted key and changed names

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from alpha_vantage.timeseries import TimeSeries
 from SP500 import SP500
 
 ## Initialize alpha vantage api
-ts = TimeSeries(key='73RE9B6EKHVY8OTJ', output_format='pandas')
+ts = TimeSeries(key='YOUR_KEY_HERE', output_format='pandas')
 
 ## Symbols dictionary for symbols page
 symbols = {'Symbols': SP500}
@@ -13,30 +13,30 @@ App = Flask(__name__)
 
 ## Symbols page
 @App.route('/Symbols/')
-def DivisionName():
+def Symbols():
     return jsonify(symbols['Symbols'])
 
 ## Main page
 @App.route("/", methods=["POST", "GET"])
 def index():
-    ## Is there a post request    
+    ## Is there a post request
     if request.method == "POST":
-        
-        ## Get symbol       
+
+        ## Get symbol
         Symbol = request.form['symbol']
-        
-        ## Get stock data 
+
+        ## Get stock data
         data, _ = ts.get_intraday(symbol=Symbol, interval='1min', outputsize='full')
         Value = data['4. close'].tolist()[0]
 
-        ## Return endpoint 
+        ## Return endpoint
         return render_template("index.html",Symbol=Symbol, Value=Value)
     else:
-        ## Return endpoint       
+        ## Return endpoint
         return render_template("index.html",Symbol=None, Value=None)
 
 
 
 ## Flask Run
 if __name__ == '__main__':
-    App.run(port=3002)
+    App.run()
